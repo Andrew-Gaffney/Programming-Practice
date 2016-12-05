0b73a171e2f8618ea677f4801495adf5ff7d16ed
Andrew Gaffney <andrew.atreides@gmail.com>
2016-12-05 16:12:13 -0500

    Solution to: https://www.freecodecamp.com/challenges/convert-html-entities

---
+++ ConvertHTML.js
@@ -0,0 +1,10 @@
+function convertHTML(str) {
+  // &colon;&rpar;
+  
+  str = str.replace(/&/g, "&amp;");
+  str = str.replace(/</g, "&lt;");
+  str = str.replace(/>/g, "&gt;");
+  str = str.replace(/'/g, "&apos;");
+  str = str.replace(/"/g, "&quot;");
+  return str;
+}
